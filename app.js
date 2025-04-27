1f8d980cf8a22872bd0096091873e772d8884c33
jon2502 <jonbjarke25@outlook.dk>
2025-04-27 19:35:44 +0200

    Added helmet

--- app.js
+++ app.js
@@ -10,18 +10,19 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-// Start db connection
-mongoose.connect(process.env.MONGODB_URI, {dbName: process.env.MONGODB_NAME});
-const db = mongoose.connection;
-db.on('error', function (error) { console.error(error); });
-db.once('open', function () { console.log('Connected to database'); });
-
+app.use(helmet());
 app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Start db connection
+mongoose.connect(process.env.MONGODB_URI, {dbName: process.env.MONGODB_NAME});
+const db = mongoose.connection;
+db.on('error', function (error) { console.error(error); });
+db.once('open', function () { console.log('Connected to database'); });
 
 app.use('/', indexRouter);
 
